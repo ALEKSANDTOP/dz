d25a09edc0ca34d3294105763756367506b6e595
Aleksandr <caha2605bychkov@gmail.com>
2023-08-07 14:44:03 +0300

    написал 2 код

---
+++ dz2/Program.cs
@@ -0,0 +1,40 @@
+﻿/*: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+
+
+
+Console.WriteLine("Введите первую цифру: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите вторую цифру: ");
+int numberB = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третию цифру: ");
+int numberC = int.Parse(Console.ReadLine());
+
+int max = numberA;
+
+if (numberA > max)
+{
+    max = numberA;
+}
+if (numberB > max)
+{
+    max = numberB;
+}
+else
+{
+    max = max;
+}
+if (numberC > max) 
+{
+    max = numberC;
+}
+else
+{
+    max = max;
+}
+
+Console.WriteLine( $"Максимальное число {max}");
+
